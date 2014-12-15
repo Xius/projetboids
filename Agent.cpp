6782e936d084bf110a929acbe9123df1e9b7a050
Piat Alexis <apiat@bs406-s16-07.insa-lyon.fr>
2014-12-15 16:45:53 +0100

    create default constructor with rand position x, y and position and speed getter

--- Agent.cpp
+++ Agent.cpp
@@ -17,7 +17,8 @@
 //                                 Project Files
 // ===========================================================================
 #include "Agent.h"
-
+#include <ctime>
+#include "param.h"
 
 
 
@@ -36,6 +37,14 @@
 // ===========================================================================
 Agent::Agent(void)
 {
+	//width = x, height = y
+	srand(time(NULL));
+	position = new double[2];
+	position[0] = (rand()/(double)RAND_MAX)*WIDTH;
+	position[1] = (rand()/(double)RAND_MAX)*HEIGHT;
+	speed = new double[2];
+	speed[0]= 1;
+	speed[1]= 1;
 }
 
 // ===========================================================================
@@ -43,12 +52,21 @@ Agent::Agent(void)
 // ===========================================================================
 Agent::~Agent(void)
 {
+	delete position;
+	delete speed;
 }
 
 // ===========================================================================
 //                                 Public Methods
 // ===========================================================================
-
+double* Agent::get_position(void)
+{
+	return position;
+}
+double* Agent::get_speed(void)
+{
+	return speed;
+}
 // ===========================================================================
 //                                Protected Methods
 // ===========================================================================
