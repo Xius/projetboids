4b11fc2f03447377a328bf066234c85809492d99
Piat Alexis <apiat@bs406-s16-07.insa-lyon.fr>
2014-12-15 17:45:19 +0100

    add test method near which says if Agent is too close to previous Agent

--- testboid.cpp
+++ testboid.cpp
@@ -13,6 +13,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include "Agent.h"
+#include <ctime>
 //#include "bwindows.h"
 
 
@@ -38,8 +39,11 @@
 // ===========================================================================
 int main(int argc, char* argv[])
 {
+  srand(time(NULL));
   Agent* bond = new Agent();
+  Agent* romanoff = new Agent();
   printf("x = %lf, y = %lf\n", bond->get_position()[0], bond->get_position()[1]);
+  printf("x = %d\n", bond->near(*romanoff));
   return 0;
 }
 
